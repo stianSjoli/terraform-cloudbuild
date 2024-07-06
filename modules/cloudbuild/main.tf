779368ccbf2502ec77efbbc3729f287fd67aa7a6
stianSjoli <stianSjoli@users.noreply.github.com>
2024-07-06 12:03:18 +0200

    Update main.tf

--- modules/cloudbuild/main.tf
+++ modules/cloudbuild/main.tf
@@ -1,15 +1,29 @@
-
-resource "google_service_account" "cloudbuild_service_account" {
-  account_id = "cloud-sa"
+resource "google_service_account" "cb_sa" {
+  account_id = "build-sa"
 }
 
-resource "google_cloudbuild_trigger" "filename_trigger" {
-  location = "us-central1"
-
+resource "google_cloudbuild_trigger" "cb_trigger" {
   trigger_template {
     branch_name = "main"
-    repo_name   = "my-repo"
+    repo_name   = "binary-authz-cloudbuild"
   }
-  service_account = google_service_account.cloudbuild_service_account.id
-  filename = "cloudbuild.yaml"
+
+  service_account = google_service_account.cb_sa.id
+  filename        = "cloudbuild.yaml"
+  depends_on = [
+    google_project_iam_member.act_as,
+    google_project_iam_member.logs_writer
+  ]
+}
+
+resource "google_project_iam_member" "act_as" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.cb_sa.email}"
+}
+
+resource "google_project_iam_member" "logs_writer" {
+  project = var.project_id
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account_cb_sa.email}"
 }
