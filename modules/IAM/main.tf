3b0f7736abb2f3e2dfa76ade11346a65f4243660
stianSjoli <stianSjoli@users.noreply.github.com>
2024-07-08 23:40:31 +0200

    Update main.tf

--- modules/IAM/main.tf
+++ modules/IAM/main.tf
@@ -1,6 +1,5 @@
-resource "google_project_iam_custom_role" "cloudbuild-custom-role" {
-  role_id     = "cloudbuild-role"
-  title       = "cloudbuild Custom Role"
-  description = "A description"
-  permissions = ["iam.roles.list"]
+
+
+module "custom-roles" {
+  source  = "./custom-roles/"
 }
