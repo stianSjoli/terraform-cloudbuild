1a6ef132ae1fa6f3e3eb662d2181ccab41f42627
stianSjoli <stianSjoli@users.noreply.github.com>
2024-07-08 21:47:47 +0200

    Update main.tf

--- environments/platform/main.tf
+++ environments/platform/main.tf
@@ -3,7 +3,11 @@ provider "google" {
   project = var.project_id
 }
 
-module "cloudbuild" {
-  source  = "../../modules/cloudbuild"
+# module "cloudbuild" {
+#  source  = "../../modules/cloudbuild"
+# }
+
+module "iam" {
+  source  = "../../modules/IAM"
 }
 
