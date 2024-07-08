deba18c422acd2d3651e7c34011b420ed02c7bf3
stianSjoli <stianSjoli@users.noreply.github.com>
2024-07-08 21:46:15 +0200

    Create main.tf

---
+++ modules/IAM/main.tf
@@ -0,0 +1,6 @@
+resource "google_project_iam_custom_role" "cloudbuild-custom-role" {
+  role_id     = "cloudbuild-role"
+  title       = "cloudbuild Custom Role"
+  description = "A description"
+  permissions = ["iam.roles.list"]
+}
