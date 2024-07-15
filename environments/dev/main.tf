f56081018b85b569692c1b01912e37b51b0a4c5a
stianSjoli <stianSjoli@users.noreply.github.com>
2024-07-15 23:51:59 +0300

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,37 +1,8 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-locals {
-  env = "dev"
-}
 
 provider "google" {
-  project = "${var.project}"
+  project = var.project_id
 }
 
-module "project-factory" {
-  source  = "terraform-google-modules/project-factory/google"
-  version = "~> 15.0"
-
-  name                 = "project-sandbox"
-  random_project_id    = true
-  usage_bucket_name    = "usage-report-bucket"
-  usage_bucket_prefix  = "integration"
-  billing_account      = "My Billing Account 1"
-  activate_apis        = ["cloudbilling.googleapis.com"]
+module "cloudstorage" {
+  source  = "../../modules/cloudstorage"
 }
-
-
-
